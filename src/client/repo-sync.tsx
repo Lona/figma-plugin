96902594130ec7c793a07964ba6e7d7cac935d73
Mathieu Dutour <mathieu@dutour.me>
2020-02-25 12:35:41 +0100

    fetch latest version directly

--- src/client/repo-sync.tsx
+++ src/client/repo-sync.tsx
@@ -1,7 +1,4 @@
 import * as React from "react";
-import { useLazyQuery } from "@apollo/react-hooks";
-import * as Octokit from "@octokit/rest";
-import { remoteAPI, GetInstallationTokenType } from "../remote-api";
 import { figmaApi } from "../figma-api";
 import { ConvertedWorkspace } from "../tokens";
 
@@ -17,73 +14,45 @@ export const RepoSync = ({
   const [githubError, setGithubError] = React.useState(null);
   const [githubLoading, setGithubLoading] = React.useState(false);
 
-  const [
-    loadInstallationToken,
-    { called, loading, error, refetch }
-  ] = useLazyQuery<GetInstallationTokenType>(remoteAPI.getInstallationToken, {
-    variables: { repo: repo.url },
-    context: {
-      headers: {
-        Authorization: "Bearer " + token
-      }
-    },
-    async onCompleted(data) {
-      if (githubLoading) {
-        console.log("already loading");
-        return;
-      }
-      setGithubLoading(true);
-      const github = new Octokit({ auth: data.getRepo.installationToken });
-
-      const params = {
-        owner: repo.url.replace("https://github.com/", "").split("/")[0],
-        repo: repo.url.replace("https://github.com/", "").split("/")[1]
-      };
-
-      try {
-        const release = await github.repos.getLatestRelease(params);
-
-        const tagName = release && release.data && release.data.tag_name;
-
-        if (!tagName) {
-          throw new Error("could not find the tag in the latest release");
-        }
-
-        const res: ConvertedWorkspace = await (
-          await fetch(
-            `${process.env.S3_URL}/${organisation.id}/refs/tags/${tagName}/flat-json.json`
-          )
-        ).json();
-
-        await figmaApi.importTokens(res);
-        setGithubLoading(false);
-      } catch (err) {
-        setGithubLoading(false);
-        setGithubError(err);
-      }
+  const importTokens = React.useCallback(async () => {
+    if (githubLoading) {
+      console.log("already loading");
+      return;
     }
-  });
+    setGithubLoading(true);
 
-  if (loading || githubLoading) {
+    try {
+      const res: ConvertedWorkspace = await (
+        await fetch(
+          `${process.env.S3_URL}/${organisation.id}/flat-tokens.json`,
+          {
+            headers: {
+              Authorization: "Bearer " + token
+            }
+          }
+        )
+      ).json();
+
+      await figmaApi.importTokens(res);
+      setGithubLoading(false);
+    } catch (err) {
+      setGithubLoading(false);
+      setGithubError(err);
+    }
+  }, []);
+
+  if (githubLoading) {
     return <p>Loading ...</p>;
   }
 
-  if (error || githubError) {
+  if (githubError) {
     return (
       <div>
-        <pre>{JSON.stringify(error || githubError, null, "  ")}</pre>
-        <button onClick={refetch}>Try Again</button>
+        <pre>{JSON.stringify(githubError, null, "  ")}</pre>
+        <button onClick={importTokens}>Try Again</button>
       </div>
     );
   }
 
-  const onClick = () => {
-    if (called) {
-      refetch();
-    } else {
-      loadInstallationToken();
-    }
-  };
-
-  return <button onClick={onClick}>Sync</button>;
+  return <button onClick={importTokens}>Sync</button>;
 };
